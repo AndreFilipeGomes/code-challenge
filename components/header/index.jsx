a6f9d267f4d23c3996d20b3e210a7e3f55ab77f4
Andre Gomes <Andre.Gomes@emea.merkleinc.com>
2023-04-06 00:29:43 +0100

    Languages implementation

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,10 +1,13 @@
+import { languages } from "../../context/languageContext";
 import Select from "../select";
 import styles from "./Header.module.css";
 
 export default function Header({ title }) {
   return (
-    <div className={styles.header_container}>
-      <Select></Select>
+    <div className={styles.container}>
+      <div className={styles.header_container}>
+        <Select data={languages}></Select>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Header from "../components/header";
+import styles from "../styles/Home.module.css";
+
+import { LanguageProvider } from "../context/languageContext";
+
 import "@tailwindcss/ui/dist/tailwind-ui.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <div className={styles.container}>
-        <Head>
-          <title>Code Challange</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </div>
+      <LanguageProvider>
+        <Header />
+        <div className={styles.container}>
+          <Head>
+            <title>Code Challange</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        </div>
+      </LanguageProvider>
     </>
   );
 }
